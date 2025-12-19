add9887880b032a86a90626653df7059663f6db6
VikramMahtoDesign <mahtovikram99@gmail.com>
2025-12-19 10:40:30 +0000

    Deploying to gh-pages from @ VikramMahtoDesign/vikrammahtodesign.github.io@4c9b223efa3df76f5fc757a61cc36cc9f500a264 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "Projects",
-          description: "Key engineering and automation projects.",
+          description: "Automation projects.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
